561efb088157cee56369cd31b01976f622f14507
Xiaoyun Yang <yangx232@gmail.com>
2019-05-14 16:05:39 -0400

    improved workflowStep code org

--- src/components/WorkflowStep.tsx
+++ src/components/WorkflowStep.tsx
@@ -14,6 +14,11 @@ import styles from './styles/workflowVis.module.css';
 
 const TRUNCATE_WORDS_CUTOFF = 10;
 
+export const truncateName = (name: string, truncateCutoff: number = TRUNCATE_WORDS_CUTOFF) =>
+    name.length > truncateCutoff
+        ? `${name.substring(0, truncateCutoff)}...`
+        : name;
+
 const Icon = ({ icon }: { icon: string }) => (
     <div className={styles.iconContainer}>
         <i className={iconClassName[icon]} />
@@ -35,46 +40,54 @@ export default class WorkflowStep extends React.PureComponent<PropsT, State>  {
         this.state = {
             dropdownMenuOpened: false
         };
+        this.boundToggleDropdownMenu = this.toggleDropdownMenu.bind(this);
+    }
+    boundToggleDropdownMenu: () => void;
+    toggleDropdownMenu() {
+        this.setState((state: State) => ({ dropdownMenuOpened: !state.dropdownMenuOpened }));
+    }
+    renderDisplayName({ name, isClickable }: { name: string; isClickable: boolean }) {
+        const { dropdownMenuOpened } = this.state;
+        const displayName = truncateName(name);
+        const caretDirClassName = classNames(
+            styles.caret,
+            dropdownMenuOpened ? styles.caretUp : styles.caretDown,
+            { [styles.highlighted]: dropdownMenuOpened }
+        );
+        return (
+            <div className={styles.workflowStepDisplayName}>
+                <p>
+                    {displayName}
+                    {isClickable &&
+                        <span className={classNames(styles.caret, caretDirClassName)} />
+                    }
+                </p>
+            </div>
+        );
     }
 
     renderWorkflowStep({
-        displayName,
+        name,
         type,
         isClickable
     }: {
-        displayName: string;
+        name: string;
         type: GenericTileType;
         isClickable: boolean;
     }) {
-        const { icon, theme, canEdit, canDelete } = workflowStepConfig[type];
-        const { dropdownMenuOpened } = this.state;
+        const { icon, theme } = workflowStepConfig[type];
+        const boxContainerClassName = isClickable ?
+            classNames(styles.boxContainer, styles.hoverable) : styles.boxContainer;
 
-
-        const caretDirClassName = dropdownMenuOpened
-            ? classNames(styles.caretUp, styles.highlighted)
-            : styles.caretDown;
-
-        if (!isClickable) {
-            return (
-                <div className={styles.boxContainer}>
-                    <div
-                        className={classNames(
-                            styles.box,
-                            styles.flexContainer,
-                            styles[`theme${theme}`]
-                        )}
-                    >
-                        <Icon icon={icon} />
-                        <div className={styles.workflowStepDisplayName}>
-                            <p>{displayName}</p>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
 
         return (
-            <div className={classNames(styles.boxContainer, styles.hoverable)}>
+            <div
+                role="button"
+                tabIndex={-1}
+                onKeyPress={() => { }}
+                className={boxContainerClassName}
+                onClick={this.boundToggleDropdownMenu}
+            >
                 <div
                     className={classNames(
                         styles.box,
@@ -83,14 +96,7 @@ export default class WorkflowStep extends React.PureComponent<PropsT, State>  {
                     )}
                 >
                     <Icon icon={icon} />
-                    <div className={styles.workflowStepDisplayName}>
-                        <p>
-                            {displayName}
-                            {(canEdit || canDelete) &&
-                                <span className={classNames(styles.caret, caretDirClassName)} />
-                            }
-                        </p>
-                    </div>
+                    {this.renderDisplayName({ name, isClickable })}
                 </div>
             </div>
         );
@@ -99,25 +105,10 @@ export default class WorkflowStep extends React.PureComponent<PropsT, State>  {
         const { name, type } = this.props;
         const { canEdit, canDelete } = workflowStepConfig[type];
 
-        // truncate name if too long
-        const displayName = name.length > TRUNCATE_WORDS_CUTOFF
-            ? `${name.substring(0, TRUNCATE_WORDS_CUTOFF)}...` : name;
-
-
         if (!canEdit && !canDelete) {
-            return this.renderWorkflowStep({
-                displayName,
-                type,
-                isClickable: false
-            });
+            return this.renderWorkflowStep({ name, type, isClickable: false });
         }
 
-        return (
-            this.renderWorkflowStep({
-                displayName,
-                type,
-                isClickable: true
-            })
-        );
+        return this.renderWorkflowStep({ name, type, isClickable: true });
     }
 }
