3e9f2fbde191fa01cbb0c218804d0e6cf71415d3
Xiaoyun <yangx232@gmail.com>
2020-03-11 12:02:32 -0400

    migrated Workflow to functional component

--- src/config/utils.tsx
+++ src/config/utils.tsx
@@ -4,8 +4,8 @@ import {
     NodeType
 } from "./workflowTypes";
 
-const displayWarning = true;
-
+const displayWarning = false;
+const isDisabled = false;
 export const getDisplayWarning = (workflowStep: WorkflowStep): ReactNode => (displayWarning ? "Oops! Something is wrong" : null);
 
 export const getNodeType = ({ workflowStep }: { workflowStep: WorkflowStep }): NodeType => {
@@ -17,4 +17,4 @@ export const getNextNodes = ({ actions }: WorkflowStep) => actions
     .filter((action) => action.actionType !== "REJECT")
     .map((action) => ({ id: action.nextWorkflowStepUid, primary: action.primary }));
 
-export const getIsDisabled = (workflowStep: WorkflowStep) => false;
+export const getIsDisabled = (workflowStep: WorkflowStep) => isDisabled;
--- src/lib/components/Workflow.tsx
+++ src/lib/components/Workflow.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 
 // Styles
 import style from "../styles/workflowVis.module.css";
@@ -31,46 +31,30 @@ interface Props extends OverwriteProps {
     workflow: WorkflowT;
 }
 
-export default class Workflow extends React.PureComponent<Props, StateT> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            editMode: false
-        };
-        this.boundToggleEditMode = this.toggleEditMode.bind(this);
-    }
+const Workflow = ({ workflow, warningIcon }: Props) => {
+    const [editMode, setEditMode] = useState(false);
+    const { workflowUid, workflowName, workflowSteps } = workflow;
 
-    boundToggleEditMode: () => void;
-
-    toggleEditMode() {
-        const { editMode } = this.state;
-        this.setState({ editMode: !editMode });
-    }
-
-
-    render() {
-        const { workflow, warningIcon } = this.props;
-        const { workflowUid, workflowName, workflowSteps } = workflow;
-
-        const { editMode } = this.state;
-        const toggleEditModeLabel = editMode ? "Done" : "Edit";
-        const toggleEditClassName = editMode ? "toggleEditHighlight" : "toggleEdit";
-        return (
-            <div>
-                <div className={style.flexContainer}>
-                    <h1 style={{ maxWidth: 200 }}>{workflowName}</h1>
-                    <button type="button" className={style[toggleEditClassName]} onClick={this.boundToggleEditMode}>
-                        {toggleEditModeLabel}
-                    </button>
-                </div>
-
-                <WorkflowVisContainer
-                    workflowUid={workflowUid}
-                    warningIcon={warningIcon || DefaultWarningIcon}
-                    workflowSteps={workflowSteps}
-                    editMode={editMode}
-                />
+    const toggleEditModeLabel = editMode ? "Done" : "Edit";
+    const toggleEditClassName = editMode ? "toggleEditHighlight" : "toggleEdit";
+    const toggleEditMode = () => setEditMode(!editMode);
+    return (
+        <div>
+            <div className={style.flexContainer}>
+                <h1 style={{ maxWidth: 200 }}>{workflowName}</h1>
+                <button type="button" className={style[toggleEditClassName]} onClick={toggleEditMode}>
+                    {toggleEditModeLabel}
+                </button>
             </div>
-        );
-    }
-}
+
+            <WorkflowVisContainer
+                workflowUid={workflowUid}
+                warningIcon={warningIcon || DefaultWarningIcon}
+                workflowSteps={workflowSteps}
+                editMode={editMode}
+            />
+        </div>
+    );
+};
+
+export default Workflow;
